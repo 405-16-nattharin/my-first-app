ce7774291015a645c8124ae893cd0efdb3ead8db
405-16-nattharin <nattharin.ch@yupparaj.ac.th>
2026-08-07 14:36:40 +0700

    Update fill_blank_app.py

--- fill_blank_app.py
+++ fill_blank_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # 1. หัวข้อเกมจัดกึ่งกลาง
-st.markdown("<h1 style='text-align: center; color: #1E88E5;'>✏️ เกมเติมคำศัพท์ภาษาอังกฤษ🧩</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #1E88E5;'>✏️เกมเติมคำศัพท์ภาษาอังกฤษ</h1>", unsafe_allow_html=True)
 st.write("ให้นักเรียนเติมคำศัพท์ภาษาอังกฤษที่ขาดหายไปในประโยคให้ถูกต้อง")
 
 st.divider()
