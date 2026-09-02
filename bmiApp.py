11da71d826e4a3bdcf7b4dca134483e5289e5488
405-16-nattharin <nattharin.ch@yupparaj.ac.th>
2026-09-02 16:28:35 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit 
 
 #ส่วนที่ 1 หัวข้อหน้าเว็บ (Title สีแดง)
 st.markdown("<h1 style='color: red; text-align: center;'>==ตัวอย่างแอปพลิเคชัน==</h1>",unsafe_allow_html=True,)
